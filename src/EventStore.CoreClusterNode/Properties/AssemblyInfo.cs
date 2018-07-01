3afe5e317992ce783a8b17e61a307db5ee58bc23
Ben Kloosterman <ben.kloosterman@orica.com>
2018-07-01 16:03:13 +1000

    All compiling ready for test

---
+++ src/EventStore.CoreClusterNode/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("c22971e1-fb66-4ee8-8e8b-7ff86e43e864")]
